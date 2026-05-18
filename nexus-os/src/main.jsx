3edbbc5125bdf41be43fe004833db7e994802abe
Ali Jalili <mrali97jl@gmail.com>
2026-05-18 20:56:30 +0330

    Feat: create all page placeholder components with headings

--- nexus-os/src/main.jsx
+++ nexus-os/src/main.jsx
@@ -2,16 +2,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-// ۱. ایمپورت فونت Inter (وزن‌های مورد نیاز)
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
-
-// ۲. ایمپورت استایل گلوبال (که theme.css را لود می‌کند)
 import "./index.css";
-
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
