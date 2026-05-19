2087822c340fe55ceebfe18cc079b7990a338be4
Ali Jalili <mrali97jl@gmail.com>
2026-05-19 15:46:13 +0330

    Feat: implement real Supabase auth (login, logout, session check)

--- nexus-os/src/context/AuthContext.jsx
+++ nexus-os/src/context/AuthContext.jsx
@@ -1,22 +1,48 @@
 /** @format */
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import supabase from "../services/supabase.";
 
 const AuthContext = createContext(null);
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [role, setRole] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  function handleLogin() {
-    console.log("Login");
+  async function handleLogin(email, password) {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      throw error;
+    }
+
+    setUser(data.user);
+    setRole(data.user.user_metadata?.role ?? "client");
   }
 
-  function handleLogout() {
-    console.log("Logout");
+  async function handleLogout() {
+    await supabase.auth.signOut();
     setUser(null);
     setRole(null);
   }
+
+  useEffect(() => {
+    async function checkSession() {
+      await supabase.auth.getSession().then(({ data: { session } }) => {
+        if (session) {
+          setUser(session.user);
+          setRole(session.user.user_metadata?.role ?? "client");
+        }
+      });
+      setIsLoading(false);
+    }
+    checkSession();
+  }, []);
 
   return (
     <AuthContext.Provider
---
+++ nexus-os/src/services/supabase..js
@@ -0,0 +1,9 @@
+/** @format */
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://safavnifxlutntnkyzmv.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhZmF2bmlmeGx1dG50bmt5em12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNzM3MzAsImV4cCI6MjA5NDc0OTczMH0.WanQdbIf6ExColYJRNmW37D-xHOlBU1OVZeVDoelR7U";
+
+export default createClient(supabaseUrl, supabaseAnonKey);
