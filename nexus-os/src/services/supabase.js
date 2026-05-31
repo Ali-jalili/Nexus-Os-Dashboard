a70d786f5d4a46247dc75b06afd6e310a6fc0144
Ali Jalili <mrali97jl@gmail.com>
2026-05-31 18:44:16 +0330

    Fix: move supabase credentials to local env variables

--- nexus-os/src/services/supabase.js
+++ nexus-os/src/services/supabase.js
@@ -2,10 +2,9 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://safavnifxlutntnkyzmv.supabase.co";
-
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhZmF2bmlmeGx1dG50bmt5em12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNzM3MzAsImV4cCI6MjA5NDc0OTczMH0.WanQdbIf6ExColYJRNmW37D-xHOlBU1OVZeVDoelR7U";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 export default supabase;
